fda854037474e1302159376540b0ead59e176cb7
Almas Akchabayev <almeynman@gmail.com>
2019-04-28 02:04:07 +0300

    gitpkg

---
+++ src/serialization.js
@@ -0,0 +1,11 @@
+const { Buffer } = require('safe-buffer')
+
+module.exports.serialize = data =>
+  Buffer.from(
+    JSON.stringify({
+      data,
+    }),
+    'base64'
+  )
+
+module.exports.deserialize = data => JSON.parse(Buffer.from(data, 'base64').toString())
