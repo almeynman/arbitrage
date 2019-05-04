2c40a47620f184b41fa9f4e4f110585c919cc574
Almas Akchabayev <almeynman@gmail.com>
2019-05-04 23:16:47 +0300

    fix serialization

--- packages/arbitrage-lib/src/serialization.js
+++ packages/arbitrage-lib/src/serialization.js
@@ -1,11 +1,4 @@
 const { Buffer } = require('safe-buffer')
 
-module.exports.serialize = data =>
-  Buffer.from(
-    JSON.stringify({
-      data,
-    }),
-    'base64'
-  )
-
-module.exports.deserialize = data => JSON.parse(Buffer.from(data, 'base64').toString())
+module.exports.serialize = data => Buffer.from(JSON.stringify(data))
+module.exports.deserialize = data => JSON.parse(Buffer.from(data))
--- packages/arbitrage-lib/test/serialization.test.js
+++ packages/arbitrage-lib/test/serialization.test.js
@@ -2,10 +2,16 @@
 
 const test = require('ava')
 const Buffer = require('safe-buffer').Buffer
-const { serialize } = require('../src')
+const { serialize, deserialize } = require('../src')
 
 test('serializes payload', t => {
   const data = { message: 'message' }
   const actual = serialize(data)
-  t.deepEqual(Buffer.from(JSON.stringify({ data }), 'base64'), actual)
+  t.deepEqual(Buffer.from(JSON.stringify(data)), actual)
+})
+
+test('deserializes payload', t => {
+  const data = serialize({ message: 'message' })
+  const actual = deserialize(data)
+  t.deepEqual({ message: 'message' }, actual)
 })
