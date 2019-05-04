4fe4f242e0a61dffbdcb53cb6cbb614cba878860
Almas Akchabayev <almeynman@gmail.com>
2019-05-04 23:35:38 +0300

    fix serialization

--- packages/arbitrage-lib/src/serialization.js
+++ packages/arbitrage-lib/src/serialization.js
@@ -1,4 +1,4 @@
 const { Buffer } = require('safe-buffer')
 
 module.exports.serialize = data => Buffer.from(JSON.stringify(data))
-module.exports.deserialize = data => JSON.parse(Buffer.from(data), 'base64')
+module.exports.deserialize = data => JSON.parse(Buffer.from(data, 'base64'))
