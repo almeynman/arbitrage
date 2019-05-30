644d572dd7b7db7bc6d79f794257a192c68e4665
Almas Akchabayev <almeynman@gmail.com>
2019-05-30 08:24:13 +0300

    remove built folder files

--- packages/core/built/order.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Order = /** @class */ (function () {
-    function Order(price) {
-        this.price = price;
-    }
-    return Order;
-}());
-exports.default = Order;
