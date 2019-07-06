42ee2a26642f3acc7495b25a25db6bb6127e0652
Almas Akchabayev <almeynman@gmail.com>
2019-07-06 15:23:27 +0300

    initial integration with dynamodb

--- application/dynamodb-opportunity-repository.test.ts
+++ application/dynamodb-opportunity-repository.test.ts
@@ -1,7 +1,7 @@
 import * as sinon from 'sinon'
 import Assessment from 'core/assessment'
 import DynamoDBOpportunityRepository from './dynamodb-opportunity-repository'
-import { DynamoDB } from 'aws-sdk';
+import { DynamoDB, Request } from 'aws-sdk';
 import * as chai from 'chai'
 import sinonChai from 'sinon-chai'
 
@@ -9,7 +9,9 @@ const expect = chai.expect;
 chai.use(sinonChai)
 
 test('puts an opportunity to a document client', async () => {
-  const docClient = sinon.createStubInstance(DynamoDB.DocumentClient)
+  const docClient = sinon.createStubInstance(DynamoDB.DocumentClient, {
+    put: (() => sinon.createStubInstance(Request)) as any
+  })
   const opportunityRepository = new DynamoDBOpportunityRepository(docClient as any)
   const opportunity = new Assessment({
     symbol: 'EUR/BTC',
@@ -25,6 +27,6 @@ test('puts an opportunity to a document client', async () => {
       expectedFee: 0.02
     },
   })
-  opportunityRepository.save(opportunity)
+  await opportunityRepository.save(opportunity)
   expect(docClient.put).to.have.been.called
 });
--- application/index.ts
+++ application/index.ts
@@ -1,3 +1,22 @@
+import DynamoDBOpportunityRepository from './dynamodb-opportunity-repository'
+import AWS from 'aws-sdk'
+import Assessment from 'core/assessment';
+
 export const helloWorld = async (event: any) => {
-    console.log('it works')
+    const opportunityRepository = new DynamoDBOpportunityRepository(new AWS.DynamoDB.DocumentClient())
+    const assessment = new Assessment({
+        symbol: 'EUR/BTC',
+        coefficient: 1.1,
+        buy: {
+          exchange: 'exchange1',
+          price: 8000,
+          expectedFee: 0.01
+        },
+        sell: {
+          exchange: 'exchange2',
+          price: 8100,
+          expectedFee: 0.02
+        },
+      })
+    await opportunityRepository.save(assessment)
 }
