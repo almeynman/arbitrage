2a5ae0f40fb7e7629d7a8ac71e82cc295c4b0836
Almas Akchabayev <almeynman@gmail.com>
2019-07-13 19:21:31 +0300

    WIP

--- packages/implementation/exchange-fees-deserialization.test.ts
+++
@@ -1,8 +0,0 @@
-import ExchangeFees from 'core/exchange-fees'
-
-import * as exchangeFees from './exchanges-config.json'
-
-test('should deserialize from json', () => {
-    const fee = ExchangeFees.fromJson(exchangeFees, 'kucoin')
-    expect(fee.taker).toBe(0.1)
-})
