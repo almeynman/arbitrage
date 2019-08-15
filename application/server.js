a150f0100acba7fc4fded2586021e5379a15e173
Almas Akchabayev <almeynman@gmail.com>
2019-08-15 09:29:22 +0300

    localstack sqs simple setup

--- application/server.js
+++ application/server.js
@@ -1,8 +1,4 @@
 const AWS = require('aws-sdk')
-const http = require('http')
-
-const hostname = 'localhost'
-const port = 3000
 
 AWS.config.update({
   accessKeyId: 'something',
@@ -11,37 +7,12 @@ AWS.config.update({
   logger: process.stdout,
 })
 
-const server = http.createServer((req, res) => {
-  console.log('triggered', req.url)
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World\n')
-})
+async function main() {
+  const sqs = new AWS.SQS({ endpoint: 'http://localhost:4576' })
+  const { QueueUrl } = await sqs.createQueue({ QueueName: 'testing123' }).promise()
+  await sqs.sendMessage({ QueueUrl, MessageBody: '{"hello":"world"}'}).promise()
+  const data = await sqs.receiveMessage({ QueueUrl }).promise()
+  console.log(data)
+}
 
-server.listen(port, hostname, async () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-
-  const sns = new AWS.SNS({ endpoint: 'http://localhost:4575' })
-  const topic = await sns.createTopic({ Name: 'testing123' }).promise()
-  const topicArn = topic.TopicArn
-
-  await sns
-    .subscribe({
-      TopicArn: topicArn,
-      Protocol: 'http',
-      Endpoint: 'http://localhost:3000/sns/publish',
-    })
-    .promise()
-    .then(console.log)
-    .catch(console.error)
-
-  await sns
-    .publish({
-      TopicArn: topicArn,
-      Message: '{"hello":"world"}',
-      MessageStructure: 'json'
-    })
-    .promise()
-    .then(console.log)
-    .catch(console.error)
-})
+main()
