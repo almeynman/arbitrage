9c3a22dabbaac99f5b4268212fbfb262070a2109
Almas Akchabayev <almeynman@gmail.com>
2019-08-21 08:59:41 +0300

    use localstack and sqs

--- application/src/index.ts
+++ application/src/index.ts
@@ -3,40 +3,33 @@ import AWS from 'aws-sdk'
 import ccxt from 'ccxt'
 import { Opportunist } from 'core'
 import { CCXTExchangeClient, ArbitrageCoordination } from 'implementation'
-import parseEvent from './parse-event'
-import { SNSEvent } from 'aws-lambda'
-import { ExchangeArgs } from 'implementation/arbitrage-coordination';
 import combineIntoPairs from './combine-into-pairs'
-import findCommonSymbols, { ExchangePair } from './find-common-symbols'
+import findCommonSymbols from './find-common-symbols'
 
-var sns = new AWS.SNS();
+export type SendMessageToNextQueue = (message: string) => Promise<any>
 
-export const sendExchangePairs = () => {
+export const sendExchangePairs = async (message: string, sendMessageToNextQueue: SendMessageToNextQueue) => {
+  console.log('Combining exchanges in pairs for assessment')
   let pairs = combineIntoPairs(ccxt.exchanges)
-  pairs.forEach(exchanges => {
-    sns.publish({
-      Message: JSON.stringify({ exchanges }),
-      TopicArn: process.env.DISPATCH_WITH_COMMON_SYMBOLS_TOPIC
-    }, (err, data) => {
-      if (err) console.log(err)
-      if (data) console.log(data)
-    })
-  })
+  pairs.forEach(exchanges => sendMessageToNextQueue(JSON.stringify({ exchanges })))
+  console.log('Done combining exchanges in pairs for assessment')
 }
 
-exports.dispatchWithCommonSymbols = async (event: SNSEvent) => {
-  const { exchanges } = parseEvent(event)
+export const dispatchWithCommonSymbols = async (message: string, sendMessageToNextQueue: SendMessageToNextQueue) => {
+  console.log('Dispatching exchange common symbols for assessment')
+  const { exchanges } = JSON.parse(message)
   const symbols = await findCommonSymbols(exchanges)
-  await Promise.all(symbols.map(symbol =>
-    sns.publish({
-      Message: JSON.stringify({ symbol, exchanges }), /* required */
-      TopicArn: process.env.ASSESS_ARBITRAGE_OPPORTUNITY_TOPIC
-    }).promise()
-  ));
+  await Promise.all(
+    symbols.map(
+      symbol => sendMessageToNextQueue(JSON.stringify({ symbol, exchanges }))
+    )
+  );
+  console.log('Done dispatching exchange common symbols')
 }
 
-exports.assess = async (event: SNSEvent) => {
-  const { symbol, exchanges } = parseEvent(event)
+export const assess = async (message: string) => {
+  console.log('Starting arbitrage assessment')
+  const { symbol, exchanges } = JSON.parse(message)
   const exchangeClient = new CCXTExchangeClient(ccxt)
   const opportunityRepository = new DynamoDBOpportunityRepository(new AWS.DynamoDB.DocumentClient())
 
@@ -54,5 +47,6 @@ exports.assess = async (event: SNSEvent) => {
   )
 
   await coordination.arbitrate()
-  console.log('done')
+
+  console.log('Done with arbitrage assessment')
 }
---
+++ application/src/server.ts
@@ -0,0 +1,72 @@
+import AWS from 'aws-sdk'
+import { Consumer } from 'sqs-consumer'
+import {
+  sendExchangePairs,
+  dispatchWithCommonSymbols,
+  assess,
+  SendMessageToNextQueue
+} from './index'
+
+AWS.config.update({
+  accessKeyId: 'something',
+  secretAccessKey: 'something',
+  region: 'us-east-1',
+  logger: process.stdout,
+})
+
+const sqs = new AWS.SQS()
+
+const SEND_EXCHANGE_PAIRS_QUEUE_URL = 'http://localhost:4576/queue/send-exchange-pairs'
+const DISPATCH_WITH_COMMON_SYMBOLS_QUEUE_URL = 'http://localhost:4576/queue/dispatch-with-common-symbols'
+const ASSESS_QUEUE_URL = 'http://localhost:4576/queue/assess'
+
+interface ConsumeSqsQueueParams {
+  sqs: AWS.SQS,
+  queueUrl: string,
+  handleMessage: (message: string, sendMessageToNextQueue: SendMessageToNextQueue) => Promise<void>,
+  nextQueueUrl?: string
+}
+
+consumeSqsQueue({
+  sqs,
+  queueUrl: SEND_EXCHANGE_PAIRS_QUEUE_URL,
+  handleMessage: sendExchangePairs,
+  nextQueueUrl: DISPATCH_WITH_COMMON_SYMBOLS_QUEUE_URL
+})
+consumeSqsQueue({
+  sqs,
+  queueUrl: DISPATCH_WITH_COMMON_SYMBOLS_QUEUE_URL,
+  handleMessage: dispatchWithCommonSymbols,
+  nextQueueUrl: ASSESS_QUEUE_URL
+})
+consumeSqsQueue({
+  sqs,
+  queueUrl: ASSESS_QUEUE_URL,
+  handleMessage: assess
+})
+
+function consumeSqsQueue({ sqs, queueUrl, handleMessage, nextQueueUrl }: ConsumeSqsQueueParams) {
+  const sendMessage = nextQueueUrl ? (message: string) => sqs.sendMessage({
+    QueueUrl: nextQueueUrl,
+    MessageBody: message
+  }).promise() : null
+  const app = Consumer.create({
+    queueUrl,
+    handleMessage: (message: AWS.SQS.Types.Message) => handleMessage(message.Body, sendMessage),
+    sqs
+  })
+
+  app.on('error', (err) => {
+    console.error(err.message);
+  });
+
+  app.on('processing_error', (err) => {
+    console.error(err.message);
+  });
+
+  app.on('timeout_error', (err) => {
+    console.error(err.message);
+  });
+
+  app.start();
+}
