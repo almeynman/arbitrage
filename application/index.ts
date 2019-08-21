060c43f31beebd5008202527c5b6493f009a4425
Almas Akchabayev <almeynman@gmail.com>
2019-08-21 08:21:03 +0300

    send and receive sqs messages

--- application/index.ts
+++ application/index.ts
@@ -3,49 +3,33 @@ import AWS from 'aws-sdk'
 import ccxt from 'ccxt'
 import { Opportunist } from 'core'
 import { CCXTExchangeClient, ArbitrageCoordination } from 'implementation'
-import parseEvent from './parse-event'
-import { SNSEvent } from 'aws-lambda'
 import combineIntoPairs from './combine-into-pairs'
 import findCommonSymbols from './find-common-symbols'
 
-var sns = new AWS.SNS();
+export type SendMessageToNextQueue = (message: string) => Promise<any>
 
-export const sendExchangePairs = () => {
+export const sendExchangePairs = async (message: string, sendMessageToNextQueue: SendMessageToNextQueue) => {
   console.log('Combining exchanges in pairs for assessment')
-
   let pairs = combineIntoPairs(ccxt.exchanges)
-  pairs.forEach(exchanges => {
-    sns.publish({
-      Message: JSON.stringify({ exchanges }),
-      TopicArn: process.env.DISPATCH_WITH_COMMON_SYMBOLS_TOPIC
-    }, (err, data) => {
-      if (err) console.log(err)
-      if (data) console.log(data)
-    })
-  })
-
+  pairs.forEach(exchanges => sendMessageToNextQueue(JSON.stringify({ exchanges })))
   console.log('Done combining exchanges in pairs for assessment')
 }
 
-export const dispatchWithCommonSymbols = async (event: SNSEvent) => {
+export const dispatchWithCommonSymbols = async (message: string, sendMessageToNextQueue: SendMessageToNextQueue) => {
   console.log('Dispatching exchange common symbols for assessment')
-
-  const { exchanges } = parseEvent(event)
+  const { exchanges } = JSON.parse(message)
   const symbols = await findCommonSymbols(exchanges)
-  await Promise.all(symbols.map(symbol =>
-    sns.publish({
-      Message: JSON.stringify({ symbol, exchanges }),
-      TopicArn: process.env.ASSESS_ARBITRAGE_OPPORTUNITY_TOPIC
-    }).promise()
-  ));
-
+  await Promise.all(
+    symbols.map(
+      symbol => sendMessageToNextQueue(JSON.stringify({ symbol, exchanges }))
+    )
+  );
   console.log('Done dispatching exchange common symbols')
 }
 
-export const assess = async (event: SNSEvent) => {
+export const assess = async (message: string) => {
   console.log('Starting arbitrage assessment')
-
-  const { symbol, exchanges } = parseEvent(event)
+  const { symbol, exchanges } = JSON.parse(message)
   const exchangeClient = new CCXTExchangeClient(ccxt)
   const opportunityRepository = new DynamoDBOpportunityRepository(new AWS.DynamoDB.DocumentClient())
 
--- application/parse-event.ts
+++
@@ -1,9 +0,0 @@
-import { SNSEvent } from 'aws-lambda'
-
-export interface Event {
-  [key: string]: any,
-}
-
-export default function (event: SNSEvent): Event {
-  return JSON.parse(event.Records[0].Sns.Message)
-}
